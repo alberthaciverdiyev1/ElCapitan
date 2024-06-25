14c68d4ba51c03ba656a92ef10dd0763259d8d99
ASUS <ASUS@ASUS>
2024-06-25 10:54:22 -0700

    Added Login/Register pages to admin panet

---
+++ Elcapitan.PresentationLayerAdminPanel/Controllers/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elcapitan.PresentationLayerAdminPanel.Controllers
+{
+    public class AppUser : Controller
+    {
+        public IActionResult LoginView()
+        {
+            return View("Login");
+        }
+
+        public IActionResult RegisterView() {
+            return View("Register");
+        }
+    }
+}
